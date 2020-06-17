738a427c2c0965fb23c66a0df6a912c1c078f769
Fabio Zanini <fabio.zanini@fastmail.fm>
2020-06-17 14:06:15 +1000

    Test mock

---
+++ .travis_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Wheels are already tested in docker image
+if [ $DOCKER_IMAGE ]; then
+  docker run --rm -v $(pwd):/io $DOCKER_IMAGE /io/testwheels.sh
+  exit $?
+fi
+
+if [ "$TRAVIS_OS_NAME" == 'osx' ]; then
+  export PATH="$HOME/miniconda/bin:$PATH"
+  source $HOME/miniconda/bin/activate
+  conda activate travis
+  #PYTHON="$HOME/miniconda/bin/python$CONDA_PY"
+  PYTHON=$(which python)
+else
+  PYTHON=${PYTHON:-python}
+fi
+
+echo "python: ${PYTHON}"
+
+echo 'Running tests...'
+
+#echo 'General tests...'
+#${PYTHON} test/test_general.py
+#if [ $? != 0 ]; then
+#    exit 1
+#fi
+
+echo "No tests yet"
+echo 'done!'
+
