5d47c30151b90f5aef1af67f9e53031ccd7dce72
Fabio Zanini <fabio.zanini@fastmail.fm>
2020-06-17 16:52:42 +1000

    Test deployed version

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
-# try and make wheels
-if [ $DOCKER_IMAGE ]; then
-  docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/buildwheels.sh 
-  if [ $? != 0 ]; then
-      exit 1
-  fi
-  ls wheelhouse/
-  if [ $? != 0 ]; then
-      exit 1
-  fi
+if [ $TRAVIS_OS_NAME == 'osx' ]; then
+  export PATH="$HOME/miniconda/bin:$PATH"
+  source $HOME/miniconda/bin/activate
+  conda activate travis
+fi
 
-# compile normally
-else
-  if [ $TRAVIS_OS_NAME == 'osx' ]; then
-    export PATH="$HOME/miniconda/bin:$PATH"
-    source $HOME/miniconda/bin/activate
-    conda activate travis
-  fi
-  
+# We do not need to actually install anything when deploying because
+# it is a pure python package
+if [ $TRAVIS_BUILD_STAGE_NAME == "Test" ]; then
   pip install -v '.'
   if [ $? != 0 ]; then
       exit 1
   fi
-fi
 
-# OSX makes wheels as well
-if [ $TRAVIS_OS_NAME == 'osx' ]; then
-  mkdir wheelhouse
-  pip wheel . -w wheelhouse/
-  if [ $? != 0 ]; then
-      exit 1
+elif [ $TRAVIS_BUILD_STAGE_NAME == "Deploy" ]; then
+  echo "Nothing to install for deploy stage"
+
+elif [ $TRAVIS_BUILD_STAGE_NAME == "Test_deployed" ]; then
+  # only test deployed builds for a release_<sematic-version>_RC?? tag to testpypi
+  if [ -z $TRAVIS_TAG ]; then
+    echo 'No TRAVIS_TAG, exit'
+    exit 1
   fi
-  #FIXME
-  ls wheelhouse
-fi
+  TAG1=$(echo $TRAVIS_TAG | cut -f1 -d_)
+  TAG2=$(echo $TRAVIS_TAG | cut -f2 -d_)
+  TAG3=$(echo $TRAVIS_TAG | cut -f3 -d_)
+  echo "TAG1: $TAG1"
+  echo "TAG2: $TAG2"
+  echo "TAG3: $TAG3"
+  
+  if [ -z $TAG2 ]; then
+    echo 'No TAG2, exit'
+    exit 1;
+  fi
 
+ # deploy onto pypitest unless you have no RC
+ if [ -z $TAG3 ]; then
+   echo "Installing remote version from pypi"
+   pip install -v 'anndataks'
+ elif [ ${TAG3:0:2} == 'RC' ]; then
+   echo "Installing remote version from testpypi"
+   pip install -v --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple 'anndataks'
+ else
+   echo "Tag not recognized: $TRAVIS_TAG"
+   exit 1
+ fi
+
+fi
