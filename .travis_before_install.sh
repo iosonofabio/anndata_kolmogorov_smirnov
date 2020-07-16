69bac9ded1c1d0b2d3e318a0ea5aeb1161488356
Fabio Zanini <fabio.zanini@fastmail.fm>
2020-07-16 12:17:26 +1000

    -y flag on conda OSX

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -32,7 +32,7 @@ elif [ $TRAVIS_OS_NAME == 'osx' ]; then
   pip --version
 
   # Use recent numpy/scipy/pandas from conda
-  conda install -c conda-forge numpy scipy pandas
+  conda install -y -c conda-forge numpy scipy pandas anndata
 
 else
   echo "OS not recognized: $TRAVIS_OS_NAME"
